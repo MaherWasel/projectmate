8a7617c3ef2df8ea39f40b5debe9bda04de511f4
Lixsr <osama142494@gmail.com>
2024-12-05 07:01:43 +0300

    improve-image-route

--- back-end/controllers/utils.js
+++ back-end/controllers/utils.js
@@ -1,15 +1,10 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-const User = require('../models/User');
-
 module.exports.getImage = async (req, res) => {
-    const userId = req.user.id;
     try {
-        const user = await User.findById(userId);
-        res.json(user.image.url);
+        res.json(req.user.image.url);
     } catch (err) {
-        const url = "https://res.cloudinary.com/ddjfk5dyz/image/upload/v1731221129/ProfileIcon.2c20b233d476e2ef3481810a6c6828c9_akbqfx.svg";
         res.status(500).json({ message: "Unexpected Error Ocurred", error: err, url });
     }
 };
